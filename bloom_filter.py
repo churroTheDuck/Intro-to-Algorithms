97bfb7363a9926b9cda2c82bfdb4619e74ad91e5
churroTheDuck <152838803+churroTheDuck@users.noreply.github.com>
2026-05-29 13:49:28 -0700

    Added k testing

--- bloom_filter.py
+++ bloom_filter.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 import csv
 
 class BloomFilter:
-    def __init__(self, m):
+    def __init__(self, m, k):
         self.m = m
+        self.k = k
         self.bloom_filter = [0] * m
 
     def _hashes(self, element):
         hashes = []
 
-        for i in range(3):
+        for i in range(self.k):
             value = f"{element}_{i}".encode()
             h = int(hashlib.md5(value).hexdigest(), 16)
             hashes.append(h % self.m)
@@ -28,12 +29,12 @@ class BloomFilter:
                 return False
         return True
 
-def testFilter(start,end):
+def testFilterM(start,end):
     false_positives = []
     inputs = 50
 
-    for size in range(start,end):
-        filter = BloomFilter(size)
+    for size in range(start, end):
+        filter = BloomFilter(size, 3)
         fp = 0
         for i in range(inputs):
             filter.insertElement(i)
@@ -46,5 +47,25 @@ def testFilter(start,end):
     plt.ylabel("False Positives")
     plt.title("Bloom Filter False Positive Rate")
     plt.show()
+    
+def testFilterK(k):
+    false_positives = []
+    inputs = 1000
 
-testFilter(1,1000);
+    for hashing in range(0, k):
+        filter = BloomFilter(10000, hashing)
+        fp = 0
+        for i in range(inputs):
+            filter.insertElement(i)
+        for i in range(inputs, inputs * 2):    
+            if (filter.checkElement(i)):
+                fp += 1
+        false_positives.append(fp)
+    plt.plot(range(k), false_positives)
+    plt.xlabel("Bloom Filter Size")
+    plt.ylabel("False Positives")
+    plt.title("Bloom Filter False Positive Rate")
+    plt.show()
+
+#testFilterM(1,1000);
+testFilterK(50);
